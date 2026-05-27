ce264f2ce76fdc401d0d3308da710de73cf84361
arnav <arnavkulkarni127@gmail.com>
2026-05-27 22:46:35 +0530

    js for v1

---
+++ script.js
@@ -0,0 +1,5 @@
+    let toggle = document.getElementById("toggle");
+    toggle.addEventListener("click", function(){
+    let body = document.body;
+    body.classList.toggle("light-mode");
+    }); 
